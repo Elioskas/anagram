fefd4ccd1bb4907b8f0e9b291de94b690f8a8f2e
Elioskas <88896208+Elioskas@users.noreply.github.com>
2022-01-29 11:02:02 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ function anagramChecker(){
     
     let str1 = document.getElementById("text-one").value;
     let str2 = document.getElementById("text-two").value;
-    let result1 = str1.toLowerCase(str1).split("").sort().toString().replace(/[^a-z0-9]/g, "");
-    let result2 = str2.toLowerCase(str2).split("").sort().toString().replace(/[^a-z0-9]/g, "");
+    let result1 = str1.toLowerCase(str1).split("").sort().toString().replace(/[^a-z0-9šđčćž]/g, "");
+    let result2 = str2.toLowerCase(str2).split("").sort().toString().replace(/[^a-z0-9šđčćž]/g, "");
        
     
     if(str1 === ""){
